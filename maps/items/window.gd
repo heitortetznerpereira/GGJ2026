6b02c8f54e0212a6085c99b8c4788372ba340359
Heitor Tetzner Pereira <heitortetzner2015@gmail.com>
2026-01-31 10:11:51 -0300

    c

--- maps/items/window.gd
+++ maps/items/window.gd
@@ -1,2 +1,23 @@
 class_name GWindow
 extends Area2D
+
+
+enum states {
+	NORMAL,
+	DANGER,
+	BOARD
+}
+
+
+var state := states.NORMAL
+
+
+func change_state(new_state : states):
+	state = new_state
+	match state:
+		states.NORMAL:
+			pass
+		states.DANGER:
+			pass
+		states.BOARD:
+			pass
---
+++ monsters/fnaf_monster/fnaf_manager.gd
@@ -0,0 +1,10 @@
+class_name FNAFManager
+extends Node
+
+
+@export var change_timer : Timer
+@export var change_time : float = 5
+
+
+func start_fnaf():
+	pass
