afe1e1f95034cf374d887bbb998383ad5d22e570
ViniciusTarelho <vinicius.tarelho@estudante.ufjf.br>
2026-01-31 11:04:22 -0300

    adiciona scene dos outros 2 monstros

---
+++ monsters/fnaf_monster/fnaf_monster.gd
@@ -0,0 +1,22 @@
+class_name FNAFMonster
+extends Area2D
+
+
+var speed : float = 200
+var velocity : Vector2
+var player : Player
+
+func _ready() -> void:
+	player = Global.player
+
+
+func _physics_process(delta: float) -> void:
+	var dir := (player.global_position - global_position
+	).normalized()
+	velocity = dir * speed
+	global_position += velocity * delta
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		Global.map.game_over()
